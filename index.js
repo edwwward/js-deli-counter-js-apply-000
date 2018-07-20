93f47183e6afb9701e64f923d129553b359f0376
Edward Castillo <edwardcastillo@me.com>
2018-07-20 14:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function nowServing(line) {
   }
 }
 
+/*
 function currentLine(line) {
   for (let i = 0; i < line.length; i++) {
     line.push(`The line is currently: ${i + 1}. ${line[i]},`);
@@ -21,7 +22,6 @@ function currentLine(line) {
   if (line.length === 0) {
     return('The line is currently empty.')
   }
-  
   return(line);
-  
 }
+*/
